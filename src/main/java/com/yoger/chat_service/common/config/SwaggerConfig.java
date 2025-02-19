1a4f016cd51156c72682050ebc313625b295cc18
whatsgoodg <rlwjddl1596@naver.com>
2025-02-19 22:27:44 +0900

    feat: RestClient Config 및 SwaggerConfig 설정

---
+++ src/main/java/com/yoger/chat_service/common/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.yoger.chat_service.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Mog Auth Server Swagger")
+                .description("Mog 채팅, 푸쉬 서비스에 대한 API")
+                .version("1.0.0");
+    }
+}
