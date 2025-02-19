58c3a9a2e4a1d2d6893cf395de82b352bcee6a37
whatsgoodg <rlwjddl1596@naver.com>
2025-02-19 16:28:41 +0900

    feat: PushSessionStore 구현 및 테스트
    클래스 기능:
    - 알림에 대한 웹 소켓 정보 연결, 조회, 삭제
    
    테스트 내용:
    - 알림에 대한 웹 소켓 정보 연결, 조회, 삭제

---
+++ src/main/java/com/yoger/chat_service/common/constant/StompConstant.java
@@ -0,0 +1,10 @@
+package com.yoger.chat_service.common.constant;
+
+public class StompConstant {
+
+    public final static String PUSH_KEY_PREFIX = "push:";
+
+    public final static String STOMP_END_POINT = "/ws";
+    public final static String STOMP_SUP = "/sub";
+    public final static String STOMP_PUB = "/pub";
+}
