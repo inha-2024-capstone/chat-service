16779e7b43b1d99ac6e48cc7c9694124aa61c802
whatsgoodg <rlwjddl1596@naver.com>
2025-02-20 22:29:50 +0900

    feat: 유저 챗 컨트롤러 구현
    기능
    - 유저 챗 목록 가져오기
    - 유저 챗 목록 삭제

---
+++ src/main/java/com/yoger/chat_service/userchat/controller/UserChatController.java
@@ -0,0 +1,38 @@
+package com.yoger.chat_service.userchat.controller;
+
+import com.yoger.chat_service.common.response.BaseResponseBody;
+import com.yoger.chat_service.common.status.enums.SuccessStatus;
+import com.yoger.chat_service.userchat.dto.request.UserChatDelRequestDTO;
+import com.yoger.chat_service.userchat.dto.response.UserChatListResponseDTO;
+import com.yoger.chat_service.userchat.service.UserChatService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user-chat")
+public class UserChatController {
+    private final UserChatService userChatService;
+
+    @GetMapping("/all")
+    public ResponseEntity<BaseResponseBody<List<UserChatListResponseDTO>>> getAllUserChats(
+            @RequestHeader("User-Id") Long userId) {
+        List<UserChatListResponseDTO> allUserChat = userChatService.getAllUserChat(userId);
+
+        return ResponseEntity.status(SuccessStatus.OK.getHttpStatus())
+                .body(SuccessStatus.OK.getResponseBody(allUserChat));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<BaseResponseBody<Void>> deleteUserChat(@RequestBody UserChatDelRequestDTO requestDTO) {
+        userChatService.deleteUserChat(requestDTO);
+        return ResponseEntity.ok().build();
+    }
+}
