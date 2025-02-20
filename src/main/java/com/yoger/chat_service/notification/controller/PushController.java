b649535ce3d22a832bdcc107a53b8b5714e63af3
whatsgoodg <rlwjddl1596@naver.com>
2025-02-20 22:42:23 +0900

    feat: Chat 컨트롤러 구현
    기능
    - 푸쉬 읽기
    - 푸쉬 목록 가져오기

---
+++ src/main/java/com/yoger/chat_service/notification/controller/PushController.java
@@ -0,0 +1,35 @@
+package com.yoger.chat_service.notification.controller;
+
+import com.yoger.chat_service.common.response.BaseResponseBody;
+import com.yoger.chat_service.common.status.enums.SuccessStatus;
+import com.yoger.chat_service.notification.dto.request.PushReadRequestDTO;
+import com.yoger.chat_service.notification.dto.response.ChatPushesResponseDTO;
+import com.yoger.chat_service.notification.service.PushingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/push")
+public class PushController {
+    private final PushingService pushingService;
+
+
+    @GetMapping("/all")
+    public ResponseEntity<BaseResponseBody<ChatPushesResponseDTO>> findAllPushes(
+            @RequestHeader(name = "User-Id") Long userId) {
+
+        return ResponseEntity.status(SuccessStatus.OK.getHttpStatus())
+                .body(SuccessStatus.OK.getResponseBody(pushingService.queryAllPushes(userId)));
+    }
+
+    @MessageMapping("/read")
+    public void sendMessage(PushReadRequestDTO requestDTO) {
+        pushingService.readPush(requestDTO);
+    }
+}
