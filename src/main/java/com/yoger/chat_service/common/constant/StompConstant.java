e4e175877252167cf4e51e4bd205c4cb0157c89b
whatsgoodg <rlwjddl1596@naver.com>
2025-03-13 18:55:31 +0900

    refactor: Websocket path 변경 및 setAllowedOrigins 삭제
    변경 이유
    - Ingress 라우팅으로 인한 api 패스 추가
    - CORS는 게이트웨이에서 수행

--- src/main/java/com/yoger/chat_service/common/constant/StompConstant.java
+++ src/main/java/com/yoger/chat_service/common/constant/StompConstant.java
@@ -4,7 +4,7 @@ public class StompConstant {
 
     public final static String PUSH_KEY_PREFIX = "push:";
 
-    public final static String STOMP_END_POINT = "/ws";
-    public final static String STOMP_SUP = "/sub";
-    public final static String STOMP_PUB = "/pub";
+    public final static String STOMP_END_POINT = "/api/ws";
+    public final static String STOMP_SUP = "/api/sub";
+    public final static String STOMP_PUB = "/api/pub";
 }
--- src/main/java/com/yoger/chat_service/websocket/config/WebSocketConfig.java
+++ src/main/java/com/yoger/chat_service/websocket/config/WebSocketConfig.java
@@ -13,8 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint(StompConstant.STOMP_END_POINT)
-                .setAllowedOrigins("*");
+        registry.addEndpoint(StompConstant.STOMP_END_POINT);
     }
 
     @Override
